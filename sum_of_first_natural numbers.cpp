f1dfd3673001f926c0f2708447bdf87757927560
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-08-23 17:30:08 +0530

    added sum of n natural number

--- sum_of_first_natural numbers.cpp
+++ sum_of_first_natural numbers.cpp
@@ -1,3 +1,4 @@
+//using formula 
 #include<iostream>
 using namespace std;
 int main(){
@@ -6,13 +7,30 @@ int n , sum ;
 sum =0;
 cout <<" Enter a number " << endl;
 cin>> n;
-
-for(int i = 0; i<=n; i++){
-   
-    sum = sum + i;
-}
-
-cout << sum;
+cout << n*(n+1)/2;
 
 return 0;
 }
+
+// using loop
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+// int n , sum ;
+// sum =0;
+// cout <<" Enter a number " << endl;
+// cin>> n;
+
+// for(int i = 0; i<=n; i++){
+   
+//     sum = sum + i;
+// }
+
+// cout << sum;
+
+// return 0;
+// }
+
