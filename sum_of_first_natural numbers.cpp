191d87cc358ba2a6bb1707ce097ac58396aa70e8
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-08-23 17:25:15 +0530

    added sum of n natural number

---
+++ sum_of_first_natural numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n , sum ;
+sum =0;
+cout <<" Enter a number " << endl;
+cin>> n;
+
+for(int i = 0; i<=n; i++){
+   
+    sum = sum + i;
+}
+
+cout << sum;
+
+return 0;
+}
