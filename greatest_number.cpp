5e4764057a82c5ec3785277185efa6b2b0e0c357
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-08-24 06:49:00 +0530

    added greatest among two  number

--- greatest_number.cpp
+++ greatest_number.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
-uaing namespace std;
+using namespace std;
 int main(){
-    
+
+int m, n;
+
+cout<<"Enter First number " << endl;
+cin>>m;
+cout<<"Enter second number "<<endl;
+cin>>n;
+
+if(m>n){
+    cout<< m << " " << "is greater than" << " " << n <<endl;
+}
+else{
+     cout<< n << " " << "is greater than" << " " << m <<endl;
+}
 }
