133a2967c3dd1840e97598aa454b9c6f362580cf
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-08-24 08:20:07 +0530

    added prime number in a given range

---
+++ prime_number_in_a_given_range.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2) {
+        return false; 
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
+int main() {
+    int start, end;
+
+   
+    cout << "Enter the starting number: ";
+    cin >> start;
+    cout << "Enter the ending number: ";
+    cin >> end;
+
+    cout << "Prime numbers between " << start << " and " << end << " are:\n";
+
+    
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
