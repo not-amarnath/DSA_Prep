8285206e1dbd6f9895429a3dac279a03f76ba97b
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-08-26 20:46:11 +0530

    Added factors of a number

---
+++ basics/factors_of_a_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n ;
+    cout<<"Enter any number " << endl;
+    cin>>n;
+
+    cout<<"Factors of   " << n << " are " << endl;
+
+    for(int i = 1; i<=n; i++){
+        if(n%i==0){
+            cout<< i <<" ";
+        }
+    }
+
+
+    return 0;
+}
