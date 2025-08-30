d43765ee21309d332a900db243cce7ef2084179b
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-08-30 17:14:58 +0530

    added power of number

--- recursion/power_of_a_number.cpp
+++ recursion/power_of_a_number.cpp
@@ -1,21 +1,34 @@
- #include<bits/stdc++.h>
-  using namespace std;
-
- int power(int base, int x){
-
-  int result=1;
-  while(x--){
-  result *= base;
- }
-
- return result;
+#include<iostream>
+using namespace std;
+int power (int a, int b)
+{
+    int ans =1;
+    for(int i=1; i<=b; i++)
+    {
+        ans = ans*a;
+    }
+    return ans;
 }
+int power2 (int a, int b)
+{
+    int ans =1;
+    for(int i=1; i<=a; i++)
+    {
+        ans = ans*b;
+    }
+    return ans;
+}
+int main()
+{
+    int a,b;
+    cout<< "Enter the value of a : ";
+    cin>>a;
+    cout<< "Enter the value of b : ";
+    cin>>b;
+    int answer = power(a,b);
+    int answer2 = power2(a,b);
 
-  int main(){
-  int base , x ;
-  cout <<" Enter Base number  : " ;
-  cin>>base;
-  cout <<"Enter the power :  ";
-  cin>>x;
-  cout<<" After calculating the power of  "<< base << " " << "to the power " <<"" <<x <<"" <<" is "<<power(base, x);
+cout<< " power of " << a  << " to the power " << b  <<" is " << answer << endl;
+cout<< " power of " << b  << " to the power " << a <<" is " << answer2 << endl;
+    return 0;
 }
