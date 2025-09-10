beb2743d1ee9f37b245c96bd29466f0aeedf2c94
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-09-10 20:39:42 +0530

    added count even and odd elements

---
+++ array/counting_even_and_odd_elements.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) cin >> arr[i];
+
+    int even_count=0, odd_count=0;
+    for(int i=0; i<n; i++) {
+        if(arr[i] % 2 == 0)
+            even_count++;
+        else
+            odd_count++;
+    }
+    cout << "Even Elements count : " << even_count << "\nOdd Elements count : " << odd_count;
+}
