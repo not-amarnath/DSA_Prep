a385555bf14e50cc547006133708c28e069645f6
amarnath-dev-dsa <ghoshamarnath06@gmail.com>
2025-09-11 21:16:59 +0530

    added harshad number

---
+++ numbers/harshad_numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int checkHarshad(int num){
+    
+    int sum = 0;
+    int temp = num;
+    
+    while(temp != 0){
+        sum = sum + temp % 10;
+        temp /= 10;
+    }
+
+    return num % sum == 0;
+}
+
+int main ()
+{
+    int n ;
+    cout<<"Enter any number : ";
+    cin>>n;
+    
+    if(checkHarshad(n))
+        cout << n << " is a Harshad's number";
+    else
+        cout << n << " is not a Harshad's number";
+
+    return 0;
+}
